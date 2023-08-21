cdf0cde524fe18d809b4776019cf697974475157
gowtham <119024688+Gowtham00007@users.noreply.github.com>
2023-08-21 20:46:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./styles.css";
+const emojiDictionary = {
+  "🤣": "Rolling on the Floor Laughing",
+  "😋": "Face Savoring Food",
+  "🤓": "Nerd Face",
+  "😱": "Face Screaming in Fear",
+  "💦": "Sweat Droplets",
+  "❣️": "Heart Exclamation",
+  "💀": "Skull"
+};
+// console.log(Object.keys(emojiDictionary));
+const emojisweknow = Object.keys(emojiDictionary);
+
+// var username = prompt("Give me your name")
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInutHander(event) {
+    const userInpu = event.target.value;
+    var meaning = emojiDictionary[userInpu];
+
+    if (meaning === undefined) {
+      meaning = " sorry we dont have this in our database ";
+    }
+    setMeaning(meaning);
+  }
+  function emojiClickhandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1> KNOWING EMOJIS !</h1>
+      <input onChange={emojiInutHander}></input>
+      <h2> {meaning}</h2>
+      <h3>EMOJIS IN DATABASE </h3>
+      {emojisweknow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickhandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem ", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
